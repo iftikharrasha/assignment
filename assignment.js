665f36e2ad42721979d3c9f8661e7329ab991c6d
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-01-22 23:38:50 +0600

    budgetCalculator done

--- assignment.js
+++ assignment.js
@@ -2,7 +2,15 @@ function kilometerToMeter(length){
     return length*1000;
 }
 
+function budgetCalculator(watch, mobile, laptop){
+    var priceOfWatch = 50;
+    var priceOfMobile = 100;
+    var priceOfLaptop = 500;
+    return priceOfWatch*watch + priceOfMobile*mobile + priceOfLaptop*laptop;
+}
+
 var getKilo = kilometerToMeter(7);
 console.log("7 kilometer = ", getKilo, "Meter");
 
-
+var getBudget = budgetCalculator(4,3,1);
+console.log("My total budget is = ", getBudget);
