ffe942314c3b2fa80c7b730d4703ebfada7d7dd0
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-01-22 23:54:28 +0600

    hotelCost done

--- assignment.js
+++ assignment.js
@@ -6,7 +6,24 @@ function budgetCalculator(watch, mobile, laptop){
     var priceOfWatch = 50;
     var priceOfMobile = 100;
     var priceOfLaptop = 500;
-    return priceOfWatch*watch + priceOfMobile*mobile + priceOfLaptop*laptop;
+    totalBudget = priceOfWatch*watch + priceOfMobile*mobile + priceOfLaptop*laptop;
+    return totalBudget;
+}
+
+function hotelCost(cost){
+    var totalCost,mod;
+    if(cost<=10){
+        totalCost = cost*100;
+        return totalCost;
+    }else if(cost>10 && cost<20){
+        mod = cost % 10;
+        totalCost = mod*80 + 1000;
+        return totalCost;
+    }else{
+        mod = cost % 10;
+        totalCost = mod*50 + 1800;
+        return totalCost;
+    }
 }
 
 var getKilo = kilometerToMeter(7);
@@ -14,3 +31,6 @@ console.log("7 kilometer = ", getKilo, "Meter");
 
 var getBudget = budgetCalculator(4,3,1);
 console.log("My total budget is = ", getBudget);
+
+var getHotelCost = hotelCost(19);
+console.log("My hotel cost is = ", getHotelCost);
