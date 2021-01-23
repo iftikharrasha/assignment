1618ab51f4da3ddef60326cf42c4a74a9157f51a
iftikharrasha <iftikhar.rasha@northsouth.edu>
2021-01-23 20:34:28 +0600

    Handled bad inputs for bonus sections

--- assignment.js
+++ assignment.js
@@ -1,36 +1,36 @@
 //https://github.com/iftikharrasha/assignment
 
 function kilometerToMeter(length){
-    return length*1000;
+        return length*1000;
 }
 
 function budgetCalculator(watch, mobile, laptop){
+    var absWatch = Math.abs(watch);     //handling negative inputs
+    var absMobile = Math.abs(mobile);   //handling negative inputs
+    var absLaptop = Math.abs(laptop);   //handling negative inputs
+
     var priceOfWatch = 50;
     var priceOfMobile = 100;
     var priceOfLaptop = 500;
-    totalBudget = priceOfWatch*watch + priceOfMobile*mobile + priceOfLaptop*laptop;
+    totalBudget = priceOfWatch*absWatch + priceOfMobile*absMobile + priceOfLaptop*absLaptop;
     return totalBudget;
 }
 
-function hotelCost(cost){
+function hotelCost(day){
+    var absDay = Math.abs(day);         //handling negative inputs
     var totalCost,mod;
-    if(cost<=10){
-        totalCost = cost*100;
+    if(absDay<=10){
+        totalCost = absDay*100;
         return totalCost;
-    }else if(cost>10 && cost<20){
-        mod = cost % 10;
+    }else if(absDay>10 && absDay<20){
+        mod = absDay % 10;
         totalCost = mod*80 + 1000;
         return totalCost;
     }else{
-        mod = cost % 10;
+        mod = absDay % 10;
         totalCost = mod*50 + 1800;
         return totalCost;
     }
-}
-
-function megaFriend(friends){
-        var longName;
-        return longName;
 }
 
 function megaFriend(friends){
@@ -47,7 +47,11 @@ function megaFriend(friends){
 }
 
 var getKilo = kilometerToMeter(7);
-console.log("7 kilometer = ", getKilo, "Meter");
+    if(getKilo>0){
+        console.log("7 kilometer = ", getKilo, "Meter");
+    }else{
+        console.log("Distance cannot be negative or zero!");
+    }
 
 var getBudget = budgetCalculator(4,3,1);
 console.log("My total budget is = ", getBudget);
@@ -55,6 +59,6 @@ console.log("My total budget is = ", getBudget);
 var getHotelCost = hotelCost(19);
 console.log("My hotel cost is = ", getHotelCost);
 
-var friends = ["Rakibul", "Iftikharrasha", "Ramakantakamar Shaheb", "Shofiq"];
+var friends = ["Rakibul", "Iftikharrasha", "Ramakantakamar Shaheb", "Shafiq"];
 var myMegaFriend = megaFriend(friends);
 console.log("My mega friend is = ", myMegaFriend);
